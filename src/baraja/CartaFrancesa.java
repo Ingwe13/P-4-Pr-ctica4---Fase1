f9912d538e41a9f7a46fdea7afeb6b006e2cd2df
Javier Mariscal <>
2014-02-04 10:05:16 +0100

    ver 1.1

---
+++ src/baraja/CartaFrancesa.java
@@ -0,0 +1,40 @@
+package baraja;
+
+import java.util.Random;
+
+
+
+public class CartaFrancesa {
+//   Random carta = new Random(12);
+//   Random palo = new Random(4);
+   
+   
+   public static final int As = 1;
+   public static final int dos = 2;
+   public static final int tres = 3;
+   public static final int cuatro = 4;
+   public static final int cinc = 5;
+   public static final int seis = 6;
+   public static final int siete = 7;
+   public static final int ocho = 8;
+   public static final int nueve = 9;
+   public static final int J = 10;
+   public static final int Q = 11;
+   public static final int K = 12;
+    
+   public static final int Treboles = 0;
+   public static final int Picas = 1;
+   public static final int Corazones = 2;
+   public static final int Diamantes = 3;
+//   private final Random cart;
+//   private final Random pal;
+//
+//    public int getPalo(){
+//        this.palo = pal;
+//    }
+//    public getNumero(){
+//    }
+    
+            
+    
+}
